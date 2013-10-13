119926a72b91b6893c032a1c2cd6c1d32fe30b9b
tuxik <tuxik.ru@gmail.com>
2013-10-13 23:57:10 +0300

    Add install for baseline

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Initialize submodules
+#git submodule init; git submodule update;
+
+curr_dir=`pwd`
+
+
+#Set up vim
+[[ -d $HOME/.vim ]] && mv $HOME/.vim $HOME/.vim-bkp
+#ln -s $curr_dir/vim $HOME/.vim
+
+[[ -f $HOME/.vimrc ]] && mv $HOME/.vimrc $HOME/.vimrc-bkp
+#ln -s $HOME/.vim/vimrc $HOME/.vimrc
+tar -xvzf vim.tar.gz
+
