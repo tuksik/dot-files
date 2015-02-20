0cf8087bda71e6d7ac9e378b63ad29e9e3832125
tuksik <tuxik.ru@gmail.com>
2015-02-20 22:49:22 +0200

    Add bash-completion for docker containers

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ ln -s $curr_dir/.bash_aliases $HOME/.bash_aliases
 
 
 touch $HOME/.baseline_dotfiles
-apt-get -y install vim  mc curl
+apt-get update && apt-get -y install bash-completion vim  mc curl
 #Fix git prompt
 curl -o ~/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
 echo 'source ~/.git-prompt.sh' >> ~/.bashrc                                                                                                          
