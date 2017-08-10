a428146c3fa588afc93874fe2c1adeb61f50c616
tuxik <tuxik.ru@gmail.com>
2017-08-10 09:32:01 +0000

    Add new Vundle

--- install.sh
+++ install.sh
@@ -5,7 +5,9 @@
 
 curr_dir=$HOME/dot-files
 
-
+apt-get update && apt-get -y install bash-completion vim  mc curl tmux
+#Add Vundle
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 #Set up vim
 [[ -d $HOME/.vim ]] && mv $HOME/.vim $HOME/.vim-bkp
 tar -xvzf $curr_dir/vim.tar.gz 
@@ -17,10 +19,6 @@ ln -s $curr_dir/.vimrc $HOME/.vimrc
 ln -s $curr_dir/.muttrc  $HOME/.muttrc
 ln -s $curr_dir/.bash_aliases $HOME/.bash_aliases
 ln -s $curr_dir/.tmux.conf  $HOME/.tmux.conf
-
-
-touch $HOME/.baseline_dotfiles
-apt-get update && apt-get -y install bash-completion vim  mc curl tmux
 #Fix git prompt
 curl -o ~/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
 echo 'source ~/.git-prompt.sh' >> ~/.bashrc                                                                                                          
@@ -30,4 +28,5 @@ if [ -n "$(getent passwd codio)" ]; then
 else                                                                                                                                                 
     echo " codio DO NOT exist"                                                                                                                    
 fi                                                                                                                                                   
+touch $HOME/.baseline_dotfiles
 . ~/.bashrc
