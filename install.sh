d723d00304079de2568407d8d214cc8ff124bbf3
tuxik <tuxik.ru@gmail.com>
2017-08-10 10:28:08 +0000

    Create .vim dir

--- install.sh
+++ install.sh
@@ -10,7 +10,8 @@ apt-get update && apt-get -y install bash-completion vim  mc curl tmux
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 #Set up vim
 [[ -d $HOME/.vim ]] && mv $HOME/.vim $HOME/.vim-bkp
-tar -xvzf $curr_dir/vim.tar.gz 
+#tar -xvzf $curr_dir/vim.tar.gz 
+mkdir $curr_dir/.vim
 ln -s $curr_dir/.vim $HOME/.vim
 
 [[ -f $HOME/.vimrc ]] && mv $HOME/.vimrc $HOME/.vimrc-bkp
